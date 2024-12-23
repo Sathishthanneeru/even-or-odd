943ea79d5bb7a892634028cbafe771bc10b2fbb4
Sathishthanneeru <sathishthanneeru36@gmail.com>
2024-12-23 10:52:12 +0530

    new

---
+++ index.js
@@ -0,0 +1,33 @@
+// debugger;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// var a=23;
+// let b=12;
+// const c=233;
+// let str="sathish";
+let str=[1,2,3,3,4]
+// // for(let i in str)
+// for(let i of str)   
+//     {
+//     // console.log(str[i])
+//      console.log("hi" ,`2X${i}=${i*2}`)
+// }
+let arr=["sathish","sagra","shyam","harish","sonu"]
+    for (let i in arr)
+    {
+        console.log(`${+i+1}-${arr[i]}`)
+
+        
+    }
+let arr1=["sathish","sagra","shyam","harish","sonu"]
+let a=1;
+    for (let i of arr1)
+    {
+        console.log(a,"-",i);
+        a++;
+
+        
+    }
+
+
