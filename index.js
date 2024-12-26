c07977cf4b83144f9e39270d50ac8c834aab49a6
Sathishthanneeru <sathishthanneeru36@gmail.com>
2024-12-26 10:30:23 +0530

    new

--- index.js
+++ index.js
@@ -6,28 +6,148 @@
 // let b=12;
 // const c=233;
 // let str="sathish";
-let str=[1,2,3,3,4]
+// let str=[1,2,3,3,4]
 // // for(let i in str)
 // for(let i of str)   
 //     {
 //     // console.log(str[i])
 //      console.log("hi" ,`2X${i}=${i*2}`)
 // }
-let arr=["sathish","sagra","shyam","harish","sonu"]
-    for (let i in arr)
-    {
-        console.log(`${+i+1}-${arr[i]}`)
+// let arr=["sathish","sagra","shyam","harish","sonu"]
+//     for (let i in arr)
+//     {
+//         console.log(`${+i+1}-${arr[i]}`)
 
         
-    }
-let arr1=["sathish","sagra","shyam","harish","sonu"]
-let a=1;
-    for (let i of arr1)
-    {
-        console.log(a,"-",i);
-        a++;
+//     }
+// let arr1=["sathish","sagra","shyam","harish","sonu"]
+// let a=1;
+//     for (let i of arr1)
+//     {
+//         console.log(a,"-",i);
+//         a++;
 
         
-    }
+//     }
+// let stud = [
+//     {id:1, s_name:"sathish", city:"hyd"},
+//     {id:2, s_name:"sonu", city:"chennai"},
+//     {id:3, s_name:"raju", city:"knr"},
+//     {id:4, s_name:"sathish", city:"adl"} 
+// ] ;
+// for (let i in stud)
+//     {
+//         console.log(stud.s_name)
+// }
+// for (i=0;i<=10;i++);
+//     console.log(i)
+//     console.log("hi", i)
+// while(false){
+//     console.log("hello")
+// }
+// let isSubscribe=true;
+// let days=30;
+// while(isSubscribe&&days>=0){
+//     console.log("subscribed", days)
+//     days--;
+// }
+// let isRechargeDone=true;
+// let validity=45;
+// while(isRechargeDone&&validity>=0){
+//     console.log("we can make the calls")
+//     console.log(`remaing days ${validity} days`)
+//     validity--;
+// }
+// let x=1;
+// do{
+//     x++;
+//     console.log(x);
+//     x++;
+// }
+// while(x<10)
+// let isSubscribe=true;
+// let episodeNum=0;
+// do{
+//     console.log(`watch ${episodeNum} episode`)
+//     episodeNum++;
+// }
+// while(isSubscribe&&episodeNum<=10);'/
 
 
+// console.log("statement1")
+// if(true){
+//     console.log("statement2")
+
+// }
+// console.log("statement3")
+
+// let user="admin"
+// console.log("enter into the website");
+// if(user=="admin"){
+//     console.log("u can enter into the admin dashboard")
+// }
+// else{
+//     console.log("u cant able to enter into the dashboard")
+// }
+// console.log("able to access the entire dashboard")  
+
+
+// let isLogin= true;
+// let user="admin"
+// if(isLogin){
+//     console.log("login is successfully");
+//     if(user == "admin"){
+//         console.log("u can enter into the admin dashboard");
+//     } else {
+//         console.log("u cant enter into the admin dashboard");
+//     }
+// }
+//     else
+//     {
+//     console.log("able to acces entire dashborad");
+//     }
+// else {
+//         console.log("plslogin")
+//     }
+
+
+
+
+
+// let budget=5000;
+// if(budget>=10000){
+//     console.log("go by flight");
+
+// }
+// else if(budget<10000 && budget>=5000){
+//     console.log("go by bus");
+// }
+// else if(budget<5000 && budget>=2000){
+//     console.log("go by train");
+// }
+// else{
+//     console.log("pratice js");
+// }
+
+
+
+// for(i=0;i<=10;i++){
+   
+//     if(i==5){
+//      break;
+    
+//     }
+//     console.log(i*2)
+
+//  }
+
+
+
+// for(i=1;i<=10;i++){
+//     let j=i*2;
+//     if(j==6){
+//         // break;
+//         continue;
+//     }
+//     console.log(i)
+// }
--- operators.py
+++ operators.py
@@ -1,27 +1,55 @@
-print(1234 > 1234.4)
-print( 2==2 and 3==3 )
-print (2 and 3)
-print (3 and 2)
-print(3 and 0)
-print(0 and 3)
-print(4 and 0)
-print(3 and 4)
-print(0 and 0)
-print(0 and 1)
-print(1 and 1)
-print(False and 2)
-print(True and 0)
-print(2 and 3)
-print(3 or 2)
-print(2 or 0)
-print(False or 0)
-print(True or 0)
-print(-2 and 0)
-print(not 2 !=2)
-1*1+1*2+1*4+0*8+1*16
-1011
-1567
-1AEB
-coverting decimal formations
-oct to decimal 
-decimal to oct
+# print(1234 > 1234.4)
+# print( 2==2 and 3==3 )
+# print (2 and 3)
+# print (3 and 2)
+# print(3 and 0)
+# print(0 and 3)
+# print(4 and 0)
+# print(3 and 4)
+# print(0 and 0)
+# print(0 and 1)
+# print(1 and 1)
+# print(False and 2)
+# print(True and 0)
+# print(2 and 3)
+# print(3 or 2)
+# print(2 or 0)
+# print(False or 0)
+# print(True or 0)
+# print(-2 and 0)
+# print(not 2 !=2)
+# 1*1+1*2+1*4+0*8+1*16
+# 1011
+# 1567
+# 1AEB
+# coverting decimal formations
+# oct to decimal 
+# decimal to oct
+print(1&2)
+print(2&1)
+print(20 | 30)
+print(30 | 20)
+print(3 ^ 4)
+print(5 ^ 5)
+print(2 ^ 6) 
+print(12 >> 23)
+# n=7
+# if n > 0:
+#     print("gretar");
+# else:
+#     if n == 0:
+#         print("zero")
+#     else:
+#         print("less");
+# for i in range(35,51):
+#     if i % 2 == 0:
+#         print(i, "even")
+#     else:
+#         print(i, "odd")
+# list = [1,2,3,4,5]
+# for i in list:
+#     if i == 5:
+#         print("fonded")
+#     else:
+#         print("better luck next time")
+  
